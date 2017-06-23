000ae190969c50396d9d709429f6de000251b2b7
sonny <son.d.luu@gmail.com>
2017-06-23 14:36:04 -0700

    added JAWSDB required codes in connection.js file to deploy in heroku

--- confiq/connection.js
+++ confiq/connection.js
@@ -1,9 +1,23 @@
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: "localhost",
+  		user: "root",
+  		password: "thecorrs1",
+  		database: "burgers_db"
+	});
+};
+
+connection.connect();
+module.exports = connection;
+
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "thecorrs1",
   database: "burgers_db"
 });
 
