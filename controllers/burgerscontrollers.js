0fa2b7b8f57727943b9597def3f86f42f3bc1090
sonny <son.d.luu@gmail.com>
2017-06-24 11:07:45 -0700

    updated files due to wrong file names

--- controllers/burgerscontrollers.js
+++ controllers/burgerscontrollers.js
@@ -1,7 +1,7 @@
 var express = require("express");
 
 var router = express.Router();
-var burger = require("../models/burger.js");
+var burger = require("../models/burgers.js");
 
 // get route -> index
 router.get("/", function(req, res) {
